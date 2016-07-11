7fe51d9dad73dc2c9c506470b00cbabbf2143354
vezikon <vezikon@hotmail.com>
2016-07-11 13:08:21 +0200

    Use v2.5.1

--- app/src/main/java/com/example/instabug/ui/activities/GoogleMapsActivity.java
+++ app/src/main/java/com/example/instabug/ui/activities/GoogleMapsActivity.java
@@ -9,15 +9,16 @@ import android.widget.Toast;
 import com.example.instabug.R;
 import com.example.instabug.ui.views.CustomGoogleMap;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.instabug.library.Instabug;
 
-public class GoogleMapsActivity extends AppCompatActivity {
+public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    private CustomGoogleMap customGoogleMap;
+    private SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,45 +31,10 @@ public class GoogleMapsActivity extends AppCompatActivity {
                 Toast.makeText(GoogleMapsActivity.this, "File saved to be uploaded", Toast.LENGTH_SHORT).show();
             }
         });
-        setUpMapIfNeeded();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            customGoogleMap = new CustomGoogleMap(getSupportFragmentManager().findFragmentById(R.id.map).getView(), mMap);
-            // TODO add this so that instabug recognizes you have a map and show it in the screenshot
-            Instabug.addCapturableView(customGoogleMap);
-
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
+        mapFragment =
+        (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     /**
@@ -79,5 +45,15 @@ public class GoogleMapsActivity extends AppCompatActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        CustomGoogleMap customGoogleMap = new CustomGoogleMap(mapFragment.getView(), mMap);
+        // TODO add this so that instabug recognizes you have a map and show it in the screenshot
+        Instabug.addCapturableView(customGoogleMap);
+
+        setUpMap();
     }
 }
