220567191990c51b76c2685222456e6b71c62b3b
Adrian Devezin <adrian.devezin@gmail.com>
2019-04-09 14:27:06 -0400

    Convert Dependencies to AndroidX

--- app/src/main/java/com/example/instabug/BaseActivity.java
+++ app/src/main/java/com/example/instabug/BaseActivity.java
@@ -1,6 +1,6 @@
 package com.example.instabug;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MotionEvent;
 
 import com.instabug.library.InstabugTrackingDelegate;
--- app/src/main/java/com/example/instabug/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/instabug/ui/activities/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
