2f837b3fdb52e22df6d05d03107c9104798643c5
abanoub <aedwar@instabug.com>
2021-06-23 13:54:08 +0200

    Added a blocking attachment file generation logic

--- app/src/main/java/com/example/instabug/SampleApplication.java
+++ app/src/main/java/com/example/instabug/SampleApplication.java
@@ -2,6 +2,12 @@ package com.example.instabug;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
 import androidx.multidex.MultiDex;
 
 import com.instabug.apm.APM;
@@ -11,12 +17,22 @@ import com.instabug.library.InstabugColorTheme;
 import com.instabug.library.InstabugCustomTextPlaceHolder;
 import com.instabug.library.internal.module.InstabugLocale;
 import com.instabug.library.invocation.InstabugInvocationEvent;
+import com.instabug.library.model.Report;
 import com.instabug.library.ui.onboarding.WelcomeMessage;
 
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Locale;
 
 
 public class SampleApplication extends Application {
+
+    public static final String TAG = "SAMPLE_APP";
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
@@ -59,9 +75,60 @@ public class SampleApplication extends Application {
         //setting user attributes
         Instabug.setUserAttribute("USER_TYPE", "instabug user");
 
-        BugReporting.setAutoScreenRecordingEnabled(true);
+        Instabug.onReportSubmitHandler(new Report.OnReportCreatedListener() {
+            @Override
+            public void onReportCreated(Report report) {
+                // perform blocking file generation operation
+                Uri fileUri = generateLargeFile();
+                // attach the file to the report after it had been generated completely
+                report.addFileAttachment(fileUri, "attachmentFile.txt");
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            BugReporting.setAutoScreenRecordingEnabled(true);
+        }
 
         APM.setEnabled(true);
 
+    }
+
+    /**
+     * thread blocking file generation
+     * @return generated file Uri
+     */
+    private Uri generateLargeFile() {
+        Resources resources = getResources();
+        File outputFile = new File(getExternalCacheDir()+"/attachmentFile.txt");
+        InputStream resourceInputStream = resources.openRawResource(R.raw.extra);
+        OutputStream fileOutputStream = null;
+        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+        try {
+            outputFile.createNewFile();
+            fileOutputStream = new FileOutputStream(outputFile);
+            byte[] buffer = new byte[1024];
+            int readLength;
+            while ((readLength = resourceInputStream.read(buffer)) > 0) {
+                fileOutputStream.write(buffer, 0, readLength);
+            }
+        } catch (IOException ioException) {
+            Log.e(TAG, "ioException", ioException);
+        } finally {
+            tryClosingClosable(resourceInputStream);
+            tryClosingClosable(fileOutputStream);
+        }
+        return Uri.fromFile(outputFile);
+    }
+
+    private void tryClosingClosable(@Nullable Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException ioException) {
+                Log.e(TAG, "couldn't close closeable", ioException);
+            }
+        }
     }
 }
