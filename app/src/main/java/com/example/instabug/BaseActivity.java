6c2d39aecd8218f6d4c351420ddf33e1aa0d9dbf
Zak <m.zakaria.elzoghbi@gmail.com>
2022-04-07 16:46:07 +0200

    Bumping SDK version and remove deprecated calls

--- app/src/main/java/com/example/instabug/BaseActivity.java
+++ app/src/main/java/com/example/instabug/BaseActivity.java
@@ -1,9 +1,6 @@
 package com.example.instabug;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.MotionEvent;
-
-import com.instabug.library.InstabugTrackingDelegate;
 
 /**
  * Created by vezikon on 10/15/16.
@@ -11,11 +8,4 @@ import com.instabug.library.InstabugTrackingDelegate;
 
 public class BaseActivity extends AppCompatActivity {
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        //To allow instabug to track user steps
-        // and also add touches to screen recording
-        InstabugTrackingDelegate.notifyActivityGotTouchEvent(ev, this);
-        return super.dispatchTouchEvent(ev);
-    }
 }
